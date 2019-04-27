3a4ac22b2a3166183603339527cfb581e19ae249
Radek Stepan <dev@radekstepan.com>
2019-04-27 19:18:22 -0400

    working script

---
+++ example.md
@@ -0,0 +1,5 @@
+
+    All this happened,
+  more or less.
+
+One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.
--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
 const {promisify} = require('util');
 
 const ora = require('ora');
 const error = require('serialize-error');
-const glob = promisify(require('glob'));
+const truncate = require('cli-truncate');
+const slugify = require('slugify');
+
+const MAX_LENGTH = 40;
 
 (async function run() {
   const status = ora();
 
+  const cwd = process.cwd();
+  const resolve = (...args) => path.resolve(...[cwd, ...args]);
+
   try {
     status.start('Start');
 
-    const cwd = process.cwd();
-    const [pattern] = process.argv.slice(2);
-    const files = await glob(pattern, {cwd});
-
-    for (const file of files) {
-      status.text = file;
-      await new Promise(r => setTimeout(r, 1e3));
+    const [input] = process.argv.slice(2);
+    if (!input) {
+      throw 'You need to pass the `input` filename as an argument';
     }
 
-    status.succeed('Done');
+    const body = await promisify(fs.readFile)(resolve(input), 'utf8');
+
+    const a = body.search(/\S/m);
+    if (a === -1) {
+      throw 'Input file is empty';
+    }
+    const b = body.search(/\./m);
+    let title = b === -1 ? body.substr(a) : body.substr(a, b - a);
+    title = truncate(title, MAX_LENGTH, {mark: ''}).replace('…', '');
+
+    const slug = slugify(title, {
+      replacement: '_',
+      lower: true
+    });
+
+    const date = new Date().toISOString().substring(0, 10);
+
+    const output = `${date}-${slug}${path.extname(input)}`;
+
+    await promisify(fs.rename)(...[input, output].map(arg => resolve(arg)));
+
+    status.succeed(output);
     process.exit(0);
   } catch (err) {
-    status.fail(error(err));
+    status.fail(error(err).message);
     process.exit(1);
   }
 })();
